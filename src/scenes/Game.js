e6b4707439a3688884f4de6972fe4cf802daeafa
Raphaël Masse <masseraphael76@gmail.com>
2021-02-01 16:11:19 +0100

    création de u game over

--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -113,6 +113,12 @@ export default class Game extends Phaser.Scene
 
         this.horizontalWrap(this.player);
 
+        const bottomPlatform = this.findBottomMostPlatform();
+        if (this.player.y > bottomPlatform.y + 200)
+        {
+            console.log('game over');
+        }
+
     }
 
     horizontalWrap(sprite)
@@ -157,5 +163,25 @@ export default class Game extends Phaser.Scene
 
         const value = `Carrots: ${this.carrotsCollected}`;
         this.carrotsCollectedText.text = value;
+    }
+
+    findBottomMostPlatform()
+    {
+        const platforms = this.platforms.getChildren();
+        let bottomPlatform = platforms[0];
+
+        for (let i = 1; i < platforms.length; ++i)
+        {
+            const platform = platforms[i];
+
+            if(platform.y < bottomPlatform.y)
+            {
+                continue
+            }
+
+            bottomPlatform = platform;
+        }
+
+        return bottomPlatform;
     }
 }
